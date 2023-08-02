56e9847ae5773d0cd3409c851c67e4fbc027e3ac
Anvay Mathur <suggestions@megagames.me>
2023-08-02 11:43:30 +0800

    create skeleton

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl leading-tight font-bold">EduKit</h1>
+    </div>
+  );
+}
+
+export default App;
